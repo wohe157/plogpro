56c49cb572aeb96f21fdbe195be74e520266c61f
Wouter Heyvaert <23416063+wohe157@users.noreply.github.com>
2021-08-26 13:03:44 +0200

    Created a console progress bar

--- plogger/progressbar.py
+++ plogger/progressbar.py
@@ -56,3 +56,54 @@ class ProgressBar(metaclass=ABCMeta):
     @abstractmethod
     def draw(self) -> None:
         pass
+
+
+class ConsoleProgressBar(ProgressBar):
+    """A text-based progress bar for in a terminal or console
+    """
+
+    def __init__(self, nsteps: int, width: int = 70):
+        """Create an instance of a `ConsoleProgressBar`
+        
+        Arguments:
+            nsteps (int)
+                The number of steps that the progressbar will go through
+            width (int, optional)
+                The width of the progress bar
+                (default: 70)
+        """
+        super().__init__(nsteps)
+        self.width = width
+
+    def draw(self) -> None:
+        nblocks = round(self.progress() * self.width)
+        output_string  = "|" + ('■' * nblocks).ljust(self.width) + "|"
+        output_string += " {:d}/{:d}".format(self.step, self.nsteps)
+
+        elapsed = round(self.current_time - self.start_time)
+        elapsed_hours, rem = divmod(elapsed, 3600)
+        elapsed_minutes, elapsed_seconds = divmod(rem, 60)
+        estimated = round(elapsed / self.progress())
+        estimated_hours, rem = divmod(estimated, 3600)
+        estimated_minutes, estimated_seconds = divmod(rem, 60)
+
+        output_string += " - "
+        if estimated_hours > 0:
+            output_string += "{:>02d}:".format(elapsed_hours)
+        if estimated_minutes > 0 or estimated_hours > 0:
+            output_string += "{:02d}:".format(elapsed_minutes)
+        output_string += "{:02d}".format(elapsed_seconds)
+        if estimated_minutes == 0 and estimated_hours == 0:
+            output_string += "s"
+
+        output_string += "/"
+        if estimated_hours > 0:
+            output_string += "{:>02d}:".format(estimated_hours)
+        if estimated_minutes > 0 or estimated_hours > 0:
+            output_string += "{:02d}:".format(estimated_minutes)
+        output_string += "{:02d}".format(estimated_seconds)
+        if estimated_minutes == 0 and estimated_hours == 0:
+            output_string += "s"
+
+        end = '\r' if self.step < self.nsteps else '\n'
+        print(output_string, end=end, flush=True)
