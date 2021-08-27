bacfd4492ec692ef277b3d26409888f2c608c05a
Wouter Heyvaert <23416063+wohe157@users.noreply.github.com>
2021-08-27 22:01:11 +0200

    Increased coverage for ProgressBar

--- tests/test_progressbar.py
+++ tests/test_progressbar.py
@@ -13,6 +13,9 @@ class TestProgressBar(unittest.TestCase):
         self.nsteps = 5
         self.p = DummyProgressBar(self.nsteps)
 
+    def test_instanciate_abc(self):
+        self.assertRaises(TypeError, plogpro.ProgressBar, self.nsteps)
+
     def test_nsteps(self):
         self.assertEqual(self.p.nsteps, self.nsteps)
     
@@ -22,8 +25,13 @@ class TestProgressBar(unittest.TestCase):
             self.p.update()
             self.assertAlmostEqual(self.p.progress(), (i+1) / self.nsteps)
 
-    def test_step(self):
+    def test_step_increment(self):
         self.assertEqual(self.p.step, 0)
         for i in range(self.nsteps):
             self.p.update()
             self.assertEqual(self.p.step, (i+1))
+
+    def test_step_select(self):
+        self.assertEqual(self.p.step, 0)
+        self.p.update(self.nsteps)
+        self.assertEqual(self.p.step, self.nsteps)
